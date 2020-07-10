102875cec65d6aa6850968b4f99eb607747f8cc6
Stéphane Bidoul <stephane.bidoul@gmail.com>
2020-07-10 12:43:14 +0200

    Add lru_cache on get_project_name

--- src/pip_deepfreeze/project_name.py
+++ src/pip_deepfreeze/project_name.py
@@ -1,6 +1,9 @@
+from functools import lru_cache
+
 from pep517.meta import load as load_metadata  # type: ignore
 
 
+@lru_cache
 def get_project_name(root: str = ".") -> str:
     distribution = load_metadata(root)
     return str(distribution.metadata["Name"])
