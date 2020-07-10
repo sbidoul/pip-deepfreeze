1888a6f0938d7971cebc9f8d85aca10d55ecfd2d
Stéphane Bidoul <stephane.bidoul@gmail.com>
2020-07-10 14:32:16 +0200

    Keep list_depends test py2 compatible

--- tests/test_list_depends.py
+++ tests/test_list_depends.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 
 import pytest
 
@@ -7,20 +8,25 @@ HERE = os.path.dirname(__file__)
 TESTPKGS_DIR = os.path.join(HERE, "testpkgs")
 LIST_DEPENDS = os.path.join(HERE, "..", "src", "pip_deepfreeze", "_list_depends.py")
 
+# /!\ this test file must be python 2 compatible and must not rely on conftest.py /!\
+
 
 @pytest.mark.parametrize(
     "to_install, distribution, expected",
     [(["pkga"], "pkga", []), (["pkgb"], "pkgb", ["pkga"]), (["pkgb"], "pkga", [])],
 )
-def test_list_depends(to_install, distribution, expected, virtualenv_python):
+def test_list_depends(to_install, distribution, expected, tmp_path):
+    venv = tmp_path / "venv"
+    subprocess.check_call([sys.executable, "-m", "virtualenv", str(venv)])
     # We need to install pytest-cov so subprocess coverage works.
     subprocess.check_call(
-        [virtualenv_python, "-m", "pip", "install", "-f", TESTPKGS_DIR, "pytest-cov"]
+        [str(venv / "bin" / "pip"), "install", "-f", TESTPKGS_DIR, "pytest-cov"]
         + to_install
     )
     depends = (
         subprocess.check_output(
-            [virtualenv_python, LIST_DEPENDS, distribution], universal_newlines=True,
+            [str(venv / "bin" / "python"), LIST_DEPENDS, distribution],
+            universal_newlines=True,
         )
         .strip()
         .split()
