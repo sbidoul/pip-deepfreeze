0b871f7159c19f20283d1098fb41305a0af92206
Stéphane Bidoul <stephane.bidoul@gmail.com>
2020-07-10 14:57:14 +0200

    Fix for py36 and py37

--- src/pip_deepfreeze/project_name.py
+++ src/pip_deepfreeze/project_name.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 from pep517.meta import load as load_metadata  # type: ignore
 
 
-@lru_cache
+@lru_cache(maxsize=1)
 def get_project_name(root: str = ".") -> str:
     distribution = load_metadata(root)
     return str(distribution.metadata["Name"])
