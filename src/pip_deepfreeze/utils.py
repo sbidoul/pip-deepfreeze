4517a178fb7c5ffe118fc4149d63f7c09169ee7a
Stéphane Bidoul <stephane.bidoul@gmail.com>
2020-07-12 18:10:37 +0200

    Add open_with_rollback utility

--- src/pip_deepfreeze/utils.py
+++ src/pip_deepfreeze/utils.py
@@ -1,4 +1,6 @@
-from typing import List
+import contextlib
+from pathlib import Path
+from typing import IO, Any, Iterator, List
 
 
 def split_lines(s: str) -> List[str]:
@@ -6,3 +8,24 @@ def split_lines(s: str) -> List[str]:
     if not s:
         return []
     return s.split("\n")
+
+
+@contextlib.contextmanager
+def open_with_rollback(
+    filename: Path, mode: str = "w", suffix: str = ".tmp"
+) -> Iterator[IO[Any]]:
+    temp_filename = filename.with_suffix(filename.suffix + suffix)
+    assert not temp_filename.exists()
+    with open(temp_filename, mode) as f:
+        try:
+            yield f
+        except BaseException:
+            try:
+                f.close()
+                temp_filename.unlink()
+            except BaseException:
+                pass
+            raise
+        else:
+            f.close()
+            temp_filename.rename(filename)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pip_deepfreeze.utils import split_lines
+from pip_deepfreeze.utils import open_with_rollback, split_lines
 
 
 @pytest.mark.parametrize(
@@ -9,3 +9,19 @@ from pip_deepfreeze.utils import split_lines
 )
 def test_split_lines(s, expected):
     assert split_lines(s) == expected
+
+
+def test_open_with_rollback(tmp_path):
+    filename = tmp_path / "thefile"
+    with open_with_rollback(filename) as f:
+        f.write("a")
+    assert filename.exists()
+    assert filename.read_text() == "a"
+    try:
+        with open_with_rollback(filename) as f:
+            f.write("b")
+            raise RuntimeError
+    except RuntimeError:
+        assert filename.read_text() == "a"
+    else:
+        raise AssertionError("should not be here")
