260561afd2e2b9e63f76142a79ed6a078de5f9aa
Stéphane Bidoul <stephane.bidoul@gmail.com>
2020-07-26 18:10:36 +0200

    Revert "Run pep517 from an emtpy temp dir"
    This reverts commit 79ae4f8f41aced0e2ceb7ee2be624648467a1c0a.

--- src/pip_deepfreeze/project_name.py
+++ src/pip_deepfreeze/project_name.py
@@ -28,19 +28,16 @@ def get_project_name(python: str, project_root: Path) -> str:
             ]
         )
         log_info(".", nl=False)
-        # run in empty directory to avoid interferences from local .egg-info dir
-        with TemporaryDirectory() as empty_dir:
-            # TODO this uses an undocumented function of pep517
-            name = check_output(
-                [
-                    python,
-                    "-c",
-                    "from pep517.meta import load; import sys; "
-                    "sys.stdout.write(load(sys.argv[1]).metadata['Name'])",
-                    str(project_root.resolve()),
-                ],
-                env=dict(os.environ, PYTHONPATH=pep517_install_dir),
-                cwd=Path(empty_dir),
-            )
+        # TODO this uses an undocumented function of pep517
+        name = check_output(
+            [
+                python,
+                "-c",
+                "from pep517.meta import load; import sys; "
+                "sys.stdout.write(load(sys.argv[1]).metadata['Name'])",
+                str(project_root),
+            ],
+            env=dict(os.environ, PYTHONPATH=pep517_install_dir),
+        )
         log_info(" " + name)
         return name
