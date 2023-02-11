e4fae7f17e79ee12832785cee3338ab3f9319370
Stéphane Bidoul <stephane.bidoul@gmail.com>
2023-02-11 23:20:42 +0100

    Factor out pyproject.toml parsing

---
+++ src/pip_deepfreeze/pyproject_toml.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from typing import Any, MutableMapping, Optional
+
+import toml
+
+from .utils import log_info
+
+PyProjectToml = MutableMapping[str, Any]
+
+
+def load_pyproject_toml(project_root: Path) -> Optional[PyProjectToml]:
+    log_info(".", nl=False)
+    pyproject_toml_path = project_root / "pyproject.toml"
+    if not pyproject_toml_path.is_file():
+        return None
+    return toml.loads(pyproject_toml_path.read_text())
