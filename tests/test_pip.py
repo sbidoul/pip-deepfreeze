d6fea58411e58037872ee320c80e906fe8a7689f
Stéphane Bidoul <stephane.bidoul@gmail.com>
2023-04-10 19:23:41 +0200

    Fix test

--- tests/test_pip.py
+++ tests/test_pip.py
@@ -291,7 +291,10 @@ def test_pip_upgrade_vcs_url(virtualenv_python, tmp_path, capfd):
     ]
     assert "Uninstalling dependencies to update" not in capfd.readouterr().err
     # reinstall, no change but different @url syntax
-    constraints.write_text("--no-index\ntoml@git+https://github.com/uiri/toml@0.10.0")
+    constraints.write_text(
+        "--no-index\n"
+        "toml@git+https://github.com/uiri/toml@4935f616ef78c35a968b2473e806d7049eba9af1"
+    )
     pip_upgrade_project(virtualenv_python, constraints, project_root=tmp_path)
     assert list(_freeze_filter(pip_freeze(virtualenv_python))) == [
         "toml @ git+https://github.com/uiri/toml"
