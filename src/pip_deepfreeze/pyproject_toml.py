ae7de4c1b958777c5fe0a496a37a821301bbc4a3
Stéphane Bidoul <stephane.bidoul@gmail.com>
2023-05-27 10:45:43 +0200

    Use tomli instead of toml

--- src/pip_deepfreeze/pyproject_toml.py
+++ src/pip_deepfreeze/pyproject_toml.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Any, MutableMapping, Optional
 
-import toml
+from .compat import tomllib
 
 PyProjectToml = MutableMapping[str, Any]
 
@@ -10,4 +10,4 @@ def load_pyproject_toml(project_root: Path) -> Optional[PyProjectToml]:
     pyproject_toml_path = project_root / "pyproject.toml"
     if not pyproject_toml_path.is_file():
         return None
-    return toml.loads(pyproject_toml_path.read_text(encoding="utf-8"))
+    return tomllib.loads(pyproject_toml_path.read_text(encoding="utf-8"))
